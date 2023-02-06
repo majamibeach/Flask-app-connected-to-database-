431650fd19625b1ad49a39da18b591f7ca99872e
Majamibeach <benkovic_m@yahoo.com>
2023-02-06 19:40:22 +0100

    Purs

---
+++ PURS_WEB_PG/main.py
@@ -0,0 +1,113 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.secret_key = '_5#y2L"F4Q8z\n\xec]/'
+
+#db
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'query1'
+mysql = MySQL(app)
+#db
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if email == 'test@gmail.com' and password == 'test':
+            session['username'] = 'test'
+            return redirect(url_for('pocetna')), 303
+        
+        elif email == 'admin@gmail.com' and password == 'admin':
+            session['username'] = 'admin'
+            return redirect(url_for('pocetna_admin')), 303
+        
+        else:
+            return render_template('login.html', error='Uneseni su krivi korisnički podaci')
+
+
+@app.route('/', methods=['GET'])
+def pocetna():
+    print(session)
+
+    if 'username' in session:
+
+        #db
+        query = f"SELECT * FROM filament"
+        cursor = mysql.connection.cursor()
+        cursor.execute(query)
+        filament = cursor.fetchall()
+        print(filament)
+        #return f'Filamenti: {filament}', 200
+        #db
+        return render_template('index.html', filament=filament)
+
+    return redirect(url_for('login')), 303
+
+@app.route('/admin', methods=['GET'])
+def pocetna_admin():
+    print(session)
+
+    if 'username' in session:
+        return render_template('pocetna_admin.html')
+
+    return redirect(url_for('login')), 303
+
+@app.route('/new-user', methods=['GET','POST'])
+def new_user():
+    print(session)
+
+    if request.method == 'GET':
+        return render_template('new_user.html')
+    elif request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        #db
+        stringfirst = "INSERT INTO korisnik(email, password, titula) VALUES('"
+        stringmidd = "', UNHEX(SHA2('"
+        stringlast = "', 256)), 'user')"
+        query = f'{stringfirst} {email} {stringmidd} {password} {stringlast}'
+        cursor = mysql.connection.cursor()
+        cursor.execute(query)
+        mysql.connection.commit()
+        mysql.connection.close()
+        #db
+    # Provjera da li mail postoji, ako ne, dozvoli stvaranje
+    # novog korisnika
+    # if email not in db.... uzmi ove podatke i storeaj ih u db
+        return redirect(url_for('pocetna_admin')), 303
+
+    return redirect(url_for('new_user.html')), 303
+
+
+@app.route('/new-filament', methods=['GET','POST'])
+def new_filament():
+    print(session)
+
+    if request.method == 'GET':
+        return render_template('new_filament.html')
+    elif request.method == 'POST':
+        proizvodjac = request.form.get('proizvodjac')
+        boja = request.form.get('boja')
+        materijal = request.form.get('materijal')
+        promjer = request.form.get('promjer')
+        masa = request.form.get('masa')
+        datum = request.form.get('datum')
+        
+    # Provjera da li mail postoji, ako ne, dozvoli stvaranje
+    # novog korisnika
+    # if email not in db.... uzmi ove podatke i storeaj ih u db
+        return redirect(url_for('pocetna_admin')), 303
+
+    return redirect(url_for('new_filament.html')), 303
+
+if __name__ == '__main__':
+    app.run()
+
